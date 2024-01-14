c887355aae00c7fbe8afd5a0e2525a2cc62b6f02
WebKit372 <69135250+WebKit372@users.noreply.github.com>
2024-01-14 14:37:06 +0300

    project 15(1.6)

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,16 +2,19 @@ const jwt = require('jsonwebtoken');
 const TokenError = require('../errors/token-err');
 
 module.exports = (req, res, next) => {
-  const { jwt: token } = req.cookies;
-  if (!token) {
-    throw new TokenError('Необходима авторизация');
+  const { method } = req;
+  if (method !== 'OPTIONS') {
+    const { jwt: token } = req.cookies;
+    if (!token) {
+      throw new TokenError('Необходима авторизация');
+    }
+    let payload;
+    try {
+      payload = jwt.verify(token, 'secret');
+    } catch {
+      next(new TokenError('Необходима авторизация'));
+    }
+    req.user = payload;
   }
-  let payload;
-  try {
-    payload = jwt.verify(token, 'secret');
-  } catch {
-    next(new TokenError('Необходима авторизация'));
-  }
-  req.user = payload;
   return next();
 };
