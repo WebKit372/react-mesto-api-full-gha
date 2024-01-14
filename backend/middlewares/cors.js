d735554a41197a5453183e8e0ae4b99f0d09c85a
WebKit372 <69135250+WebKit372@users.noreply.github.com>
2024-01-14 15:22:02 +0300

    project 15(1.10)

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,13 +6,15 @@ const allowedCors = [
   'https://localhost:3000',
 ];
 module.exports = (req, res, next) => {
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    const { method } = req;
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+    const requestHeaders = req.headers['access-control-request-headers'];
     if (method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', requestHeaders);
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
       return res.end();
     }
   }
