5afb1bb277f6ad61f2725bd40b570ea294782816
WebKit372 <69135250+WebKit372@users.noreply.github.com>
2024-01-14 14:33:27 +0300

    project 15(1.5)

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,6 +8,7 @@ const allowedCors = [
 module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
+  console.log(requestHeaders);
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
