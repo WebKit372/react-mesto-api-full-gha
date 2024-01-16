aa7e94a4f512b6cc82f7eea9e38ebd83d3507196
Tamir Erdyneev <tamir431104@gmail.com>
2024-01-16 14:08:17 +0300

    project15(v.2.6)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -95,7 +95,8 @@ function App() {
     }
     function handleCardLike(card) {
         const isLiked = card.likes.some(i => i._id === currentUser._id);
-        
+        console.log(isLiked);
+        console.log(currentUser._id);
         apiInfo.changeLike(card._id, isLiked).then((newCard) => {
             setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
     })
